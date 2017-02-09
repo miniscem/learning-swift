20ff88d25347a0921f193c3fdf54ded994bad6c5
Mark James Minisce <mark.minisce@bnymellon.com>
2017-02-08 19:08:44 -0500

    completed build a basic ui tutorial

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Minisce, Mark on 2/8/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
